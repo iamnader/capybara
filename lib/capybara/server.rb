1b6d2e9fcdfbb3399ca418505621ebf09029d7a2
Nader Akhnoukh <iamnader@gmail.com>
2010-09-05 13:01:55 -0400

    temp hack to hardcode port

--- lib/capybara/server.rb
+++ lib/capybara/server.rb
@@ -90,12 +90,13 @@ module Capybara
 
   private
 
-    def find_available_port
-      server = TCPServer.new('127.0.0.1', 0)
-      server.addr[1]
-    ensure
-      server.close if server
-    end
+  def find_available_port
+    @port = 9887
+  #   server = TCPServer.new('127.0.0.1', 0)
+  #   server.addr[1]
+  # ensure
+  #   server.close if server
+  end
 
   end
 end
